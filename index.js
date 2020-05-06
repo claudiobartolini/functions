690cda6d97b71854a3741b6b1a8f136d49069445
Claudio Bartolini <cbartolini@box.com>
2020-05-06 13:42:44 -0700

    reverted to previous version

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const sdk = BoxSDK.getPreconfiguredInstance(boxConfig);
  *
  * The client will automatically create and refresh the service account access token, as needed.
  */
-const box = sdk.getAppAuthClient('enterprise');
+const client = sdk.getAppAuthClient('enterprise');
 
 /**
  *  YOUR CODE GOES HERE!!!
@@ -35,7 +35,7 @@ exports.handler = (req, res) => {
     console.log('Event: ' + req.body);
 
     // Get details on the current user  (the service account)
-    box.users.get(client.CURRENT_USER_ID, null, (err, result) => {
+    client.users.get(client.CURRENT_USER_ID, null, (err, result) => {
         let response;
 
         if (err) {
