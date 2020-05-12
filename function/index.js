aace918d9debb55f213d8f4d040b6555236cd26e
Claudio Bartolini <cbartolini@box.com>
2020-05-11 20:55:30 -0700

    split function file into configuration + action, moved conf into box.js

--- function/index.js
+++ function/index.js
@@ -7,23 +7,10 @@
 
 'use strict';
 
-const BoxSDK = require('box-node-sdk');
-
-// Load the config from an environment variable for security and configuration management.
-const boxConfig = JSON.parse(process.env.BOX_CONFIG);
-
-const sdk = BoxSDK.getPreconfiguredInstance(boxConfig);
-
 /**
- * Create a service account client that performs actions in the context of the specified
- * enterprise.  The app has a unique service account in each enterprise that authorizes the app.
- * The service account contains any app-specific content for that enterprise.
- * Depending on the scopes selected, it can also create and manage app users or managed users
- * in that enterprise.
- *
- * The client will automatically create and refresh the service account access token, as needed.
+ * TODO: wrap the box configuration code into a proper module 
  */
-const client = sdk.getAppAuthClient('enterprise');
+var client = require('../box.js');
 
 /**
  *  YOUR CODE GOES HERE!!!
